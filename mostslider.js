be9f9340286372078d1cc84dd25df6f7c026d807
Julian Handl <julian.handl@gmail.com>
2014-10-01 15:07:01 +0200

    packed loading of social plugins into a function and call it when they are needed. Unnecessary loadings get prevented

--- mostslider.js
+++ mostslider.js
@@ -1033,6 +1033,11 @@
         /***** INITIAL *****/
         /*******************/
         
+        // LOAD SOCIAL PLUGINS IF NEEDED
+        if(settings.socialButtons == true){
+	        getSocialPlugins();
+        }
+        
         // SHOW THE FIRST ELEMENT
         if(settings.aniMethod == 'jQuery'){
         	slider.find('#slides #' + current).css("display","block");
@@ -1093,23 +1098,25 @@ function getURLParameter(name) {
     );
 }
 
-// PINTEREST
-(function(d){
-  var f = d.getElementsByTagName('SCRIPT')[0], p = d.createElement('SCRIPT');
-  p.type = 'text/javascript';
-  p.async = true;
-  p.src = '//assets.pinterest.com/js/pinit.js';
-  f.parentNode.insertBefore(p, f);
-}(document));
-
-// FACEBOOK
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/de_DE/all.js#xfbml=1";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
-// TWITTER
-!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
+function getSocialPlugins(){
+	// PINTEREST
+	(function(d){
+	  var f = d.getElementsByTagName('SCRIPT')[0], p = d.createElement('SCRIPT');
+	  p.type = 'text/javascript';
+	  p.async = true;
+	  p.src = '//assets.pinterest.com/js/pinit.js';
+	  f.parentNode.insertBefore(p, f);
+	}(document));
+	
+	// FACEBOOK
+	(function(d, s, id) {
+	  var js, fjs = d.getElementsByTagName(s)[0];
+	  if (d.getElementById(id)) return;
+	  js = d.createElement(s); js.id = id;
+	  js.src = "//connect.facebook.net/de_DE/all.js#xfbml=1";
+	  fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));
+	
+	// TWITTER
+	!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
+}
