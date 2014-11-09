d7bf9c2fabb4ec3f3f8088352abf28a5183edb6a
julianhandl <julian.handl@gmail.com>
2014-11-09 20:26:00 +0100

    rewriting slider

--- mostslider.js
+++ mostslider.js
@@ -12,9 +12,76 @@
 	// minified (especially when both are regularly referenced in your plugin).
 	
 	// Create the defaults once
-	var pluginName = "defaultPluginName",
-			defaults = {
-			propertyName: "value"
+	var pluginName = "mostSlider",
+		defaults = {
+			// ANIMATION
+            // animation effect
+            animation: "fade",
+            // animation speed
+            aniSpeed: 1000,
+            // animation method / jQuery, css, auto
+            aniMethod: "auto",
+            // sets if animation method should consider a fallback if selected method is not supported
+            aniFallback: true,
+            // put slider image as background and center it - only avilable if metrics are set
+            background_center: false,
+            
+            // SPEED
+            // slide automatically
+            autoPlay: true,
+            // time between slide
+            pauseTime: 3000,
+            
+            // METRICS
+            // slider width and height
+            metrics: {
+            	// RATIO
+            	// max width
+	            width: 0,
+	            // max height
+	            height: 0,
+            },
+            // stay the same height during resize
+            solidHeight: false,
+            
+            // NAVIGATION
+            // use bullets as thumbnails
+            thumbnails: false,
+            // put thumbnailimage in background
+            thumb_bg: false,
+            // en/disable navigation
+            navigation: true,
+            // hide the arrows
+            hideArrows: true,
+            
+            // FUNCTION
+            // enable linking to slides via url
+            linkable: false,
+            
+            // CONTENT
+            // if content has transparancies
+            transparancy: false,
+            // specify the content class or animated content
+            contentClass: "content",
+            // proceed content animation StepByStey
+            sbsContent: false,
+            // time between each content animation step
+		    contentAniDelay: 300,
+		    
+		    // IMAGES
+		    // user different images for different slider sizes
+		    responsive_images: false,
+		    // breakpoint between mobile and tablet (min tablet)
+		    responsive_break_tablet: 481,
+		    // breakpoint between tablet and desktop (min desktop)
+            responsive_break_desktop: 1024,
+		    
+		    // enable social button for each slide
+		    socialButtons: false,
+		    // url for social buttons
+		    socialUrl: "",
+		    // twitter id
+		    twitterID: ""
 	};
 	
 	// The actual plugin constructor
@@ -27,22 +94,151 @@
 			this.settings = $.extend( {}, defaults, options );
 			this._defaults = defaults;
 			this._name = pluginName;
+			var root = this;
+			
+			this.slider = {
+				'obj': element,
+				'aniMethod': '',
+				'init_height': 0,
+				'ratio': 0,
+				'slides': 0,
+				'current_slide': 1,
+			}
+			
 			this.init();
+			
+			setInterval(function(){
+				root.next();
+			}, this.settings.pauseTime);
 	}
 	
 	// Avoid Plugin.prototype conflicts
 	$.extend(Plugin.prototype, {
 			init: function () {
-					// Place initialization logic here
-					// You already have access to the DOM element and
-					// the options via the instance, e.g. this.element
-					// and this.settings
-					// you can add more functions like the one below and
-					// call them like so: this.yourOtherFunction(this.element, this.settings).
-					console.log("xD");
+			
+				var obj = this;
+				var slider = this.slider;
+				
+				// set aniMethod and Fallback
+				
+				// wrap the slides
+				$(slider.obj).wrapInner('<div id="slides" />');
+					
+				// set slides
+				$(slider.obj).find("#slides > *").each(function(index){
+					// init css
+					switch(obj.settings.animation){
+						case 'slide':
+							// css for slides
+							$(this).css({
+								"left": 100 * index + '%',
+							});
+							break;
+						case 'fade':
+							$(this).css({
+								"opacity": 0,
+							});
+							break;
+					}
+					// get the heighest slide
+					if($(this).height() > slider.init_height){
+						slider.init_height = $(this).height();
+					}
+					// get number of slides
+					slider.slides += 1;
+					// set bg image
+					if($(this).find("img").length > 0){
+						$(this).find("img").addClass("bg");
+					}
+				});
+				
+				// set first slide
+				$(this.slider.obj).find('#slides>*:nth-child(1)').css({
+					"z-index": 10,
+					"opacity": 1
+				});
+				
+				// init css
+				switch(obj.settings.animation){
+					case 'slide':
+						$(slider.obj).find("#slides").css({
+							"transition": "left " + obj.settings.aniSpeed.toString() + "ms ease",
+						});
+						break;
+					case 'fade':
+						$(slider.obj).find("#slides>*").css({
+							"transition": "opacity " + obj.settings.aniSpeed.toString() + "ms ease",
+						});
+						break;
+				}
+				
+				// give slider the correct ratio
+				slider.ratio = (slider.init_height/$(slider.obj).width()) * 100;
+				$(slider.obj).css("padding-bottom", ( (slider.init_height/$(slider.obj).width()) * 100 ).toString() + '%');
+				
 			},
-			yourOtherFunction: function () {
-					// some logic
+			goTo: function (index) {
+			
+				var root = this;
+				var current_slide = this.slider.current_slide;
+				
+				// if index isn't out of range
+				if(index>0 && index<=this.slider.slides){
+				
+					switch(this.settings.animation){
+						case 'slide':
+							// css
+							if(this.settings.aniMethod == 'css'){
+								alert("css");
+								$(this.slider.obj).find("#slides").css("left", ((index-1)*100*(-1)).toString() + '%');
+							}
+							//jQuery
+							else if(this.settings.aniMethod == 'jQuery'){
+								alert("jquery");
+								$(this.slider.obj).find("#slides").animate({
+									"left": ((index-1)*100*(-1)).toString() + '%',
+								});
+							}
+							break;
+						case 'fade':
+							// css
+							if(this.settings.aniMethod == 'css'){
+								$(this.slider.obj).find('#slides>*:nth-child(' + this.slider.current_slide + ')').css({
+									"z-index": 0,
+								});
+								$(this.slider.obj).find('#slides>*:nth-child(' + index + ')').css({
+									"z-index": 10,
+									"opacity": 1
+								}).bind("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", function(){
+									$(root.slider.obj).find('#slides>*:nth-child(' + current_slide + ')').css({
+										"opacity": 0,
+									});
+								});
+							}
+							else if(this.settings.aniMethod == 'jQuery'){
+								
+							}
+							break;
+					}
+					
+					this.slider.current_slide = index;
+				}
+			},
+			next: function () {
+				if(this.slider.current_slide < this.slider.slides){
+					this.goTo(this.slider.current_slide + 1);
+				}
+				else if(this.slider.current_slide == this.slider.slides){
+					this.goTo(1);
+				}
+			},
+			prev: function () {
+				if(this.slider.current_slide > 1){
+					this.goTo(this.slider.current_slide - 1);
+				}
+				else if(this.slider.current_slide == 1){
+					this.goTo(this.slider.slides);
+				}
 			}
 	});
 	
